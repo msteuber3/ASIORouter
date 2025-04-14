277ef50659d38c6ae4d5abf906f813e4c439fab1
Michael <msteuber3@gmail.com>
2025-04-13 23:53:20 -0400

    error notes

--- Source/MixerSource/MainMixer.cpp
+++ Source/MixerSource/MainMixer.cpp
@@ -44,6 +44,7 @@ void MainMixer::setBusOrder(
     std::vector<std::tuple<juce::AudioChannelSet, juce::String>> outputBuses) 
 {
     std::map<int, juce::String> inputs;
+    std::map<int, juce::String> outputs;
     int totalChannelIndex = 0;
     int deviceIndex = 0;
     for (std::tuple<juce::AudioChannelSet, juce::String> IOTuple : inputBuses) {
@@ -66,36 +67,80 @@ void MainMixer::setBusOrder(
             DBG("Sorry future Michael, have fun with this; I'm feeling quite lazy");
         }
     }
+    for (std::tuple<juce::AudioChannelSet, juce::String> IOTuple : outputBuses) {
+        if (std::get<0>(IOTuple).getTypeOfChannel(0) == juce::AudioChannelSet::ChannelType::centre)
+        {
+            outputs[totalChannelIndex] = std::get<1>(IOTuple) + " Output Channel 1";
+            totalChannelIndex++;
+            deviceIndex++;
+        }
+        else if (std::get<0>(IOTuple).getTypeOfChannel(0) == juce::AudioChannelSet::ChannelType::left)
+        {
+            outputs[totalChannelIndex] = std::get<1>(IOTuple) + " Output Channel 1";
+            totalChannelIndex++;
+            outputs[totalChannelIndex] = std::get<1>(IOTuple) + " Output Channel 2";
+            totalChannelIndex++;
+            deviceIndex++;
+        }
+        else if (std::get<0>(IOTuple).getTypeOfChannel(0) == juce::AudioChannelSet::ChannelType::discreteChannel0)
+        {
+            DBG("Sorry future Michael, have fun with this; I'm feeling quite lazy");
+        }
+    }
 
 }
 
+// Create devices from the current selected driver
 void MainMixer::ScanCurrentDriver() {
 
+    // If no driver is selected, print das debug statement and return
     if (!deviceType) {
         DBG("Error: No valid device type found.");
         return;
     }
 
+    // Get the input and ouput device names as juce::StringArrays 
     deviceType->scanForDevices();
     inDeviceNames = deviceType->getDeviceNames(true);
     outDeviceNames = deviceType->getDeviceNames(false);
 
-    // Input device setup
+    // Device index
     int ind = 0;
-    int channelIndex = 0;
+    // Channel index
+ //   int channelIndex = 0;
+    // for input device name in list of input devices from deviceType's getDeviceName()
     for (const juce::String& name : inDeviceNames) {
+        // Create new AudioInputDevice (name = name, name; deviceType = current device type, ind = device index, input = true
         auto inDevice = std::make_unique<AudioInputDevice>(name, name, deviceType, ind, true);
+
+        // Create device channels, return number of input devices
         int maxIn = inDevice->createChannels(this);
 
+        // move the new device to the inputDeviecs vector
         inputDevices.push_back(std::move(inDevice));
+
+        // Increment input devices
         ind++;
 
-        for (int inChannel = 0; inChannel < maxIn; inChannel++) {
-            if (inputDevices.back()->channels[inChannel]) {  
-                inChannels.push_back(std::move(inputDevices.back()->channels[inChannel]));
-                channelIndex++;
-            }
-        }
+        // for channel in channels in the input device
+      /////////////  for (int inChannel = 0; inChannel < maxIn; inChannel++) {
+      /////////////      // InputDevices is a vector of AudioInputDevices, which each contain a vector of Channels
+      /////////////      // This checks if the channel exists
+      /////////////      if (inputDevices.back()->channels[inChannel]) {  
+      /////////////          // Then it moves that channel to the inChannels vector, REMOVING IT from the inputDevice->channels vector
+      /////////////          // AFAIK, this works fine
+      /////////////
+      /////////////          // Can we remove inChannels and make this reliant on the AudioInputChannel object?
+      /////////////          inChannels.push_back(std::move(inputDevices.back()->channels[inChannel]));
+      /////////////          //Increment the channel index
+      /////////////          channelIndex++;
+      /////////////      }
+      /////////////  }
+        // Then add the input device to the flexbox. This could be problematic, as we aren't copying the pointer; we're moving it.
+        // So inputDevices.back will not contain the slider we need (I think)
+        // inputDevices.back is the most recent input device; the component we are adding relies on the channels vector that we remove items from; 
+        // therefore, we should handle everything in AudioInputDevice here OR make sure we are creating the GUI components here at least. 
+        // NEXT STEP: Check all instances of AudioInputDevice creation and see how much we can add here
         inputComponent.addAndMakeVisible(*inputDevices.back());
         inputBox.items.add(juce::FlexItem(*inputDevices.back()).withMinHeight(250.0f).withMinWidth(DEVICE_CONTAINER_WIDTH));
     }
@@ -113,32 +158,19 @@ void MainMixer::ScanCurrentDriver() {
         outputDevices.push_back(std::move(outDevice));
         ind++;
 
-        for (int outChannel = 0; outChannel < maxOut; outChannel++) {
-            if (outputDevices.back()->channels[outChannel]) {
-                outChannels.push_back(std::move(outputDevices.back()->channels[outChannel]));
-
-            }
-        }
+     ////////////   for (int outChannel = 0; outChannel < maxOut; outChannel++) {
+     ////////////       if (outputDevices.back()->channels[outChannel]) {
+     ////////////           outChannels.push_back(std::move(outputDevices.back()->channels[outChannel]));
+     ////////////
+     ////////////       }
+     ////////////   }
         outputComponent.addAndMakeVisible(*outputDevices.back());
         outputBox.items.add(juce::FlexItem(*outputDevices.back()).withMinHeight(250.0f).withMinWidth(DEVICE_CONTAINER_WIDTH));
     }
 }
 
-//void MainMixer::addChannelToBus(int busIndex)
-//{
-//    int numChannels = getBus(true, 0)->getNumberOfChannels();
-//
-//    auto layout = getBus(true, 0)->getCurrentLayout();
-//
-//    layout.addChannel(juce::AudioChannelSet::unknown);
-//
-//    getBus(true, 0)->setCurrentLayout(layout);
-//
-//    setBusesLayout(getBus(true, 0)->getBusesLayoutForLayoutChangeOfBus(layout));
-//}
-
 std::unique_ptr<Channel> MainMixer::getChannelFromBusBuffer() {
-
+    return nullptr;
 }
 
 void MainMixer::resized() {
@@ -168,18 +200,9 @@ void MainMixer::resized() {
     mixerBox.performLayout(fbRect);
 }
 
+void MainMixer::InitializeInputDevices(){}
 
-
-void MainMixer::InitializeInputDevices()
-{
-	//auto* currentDevice = mixerDeviceManager->getCurrentAudioDevice();
-}
-
-void MainMixer::RefreshInputDevices()
-{
-
-}
-
+void MainMixer::RefreshInputDevices(){}
 
 //
 
@@ -200,22 +223,24 @@ void MainMixer::prepareToPlay(double sampleRate, int maximumExpectedSamplesPerBl
 
 void MainMixer::processBlock(juce::AudioBuffer<float>& buffer, juce::MidiBuffer& midiMessages)
 {
-    for (int busIndex = 0; busIndex < getBusCount(true); busIndex++) {
+    //So the bus corresponds to the AudioInputDevice
+    int busCount = getBusCount(true);
+    for (int busIndex = 0; busIndex < busCount; busIndex++) {
         auto bus = getBus(true, busIndex);
 
+        // ISSUE: ONLY THE FIRST LOADED BUS IS GETTING CHANNELS
         juce::AudioBuffer<float> busBuffer = bus->getBusBuffer(buffer);
 
         for (int channel = 0; channel < busBuffer.getNumChannels(); ++channel)
         {
+            // And the channel is relative to that. SO let's auto AudioInputDevice and then access the channel by index based on that
             int numSamples = busBuffer.getNumSamples();
             float* data = busBuffer.getWritePointer(getChannelIndexInProcessBlockBuffer(true, busIndex, channel));
-            inChannels[channel]->process(data, busBuffer.getNumSamples());
+            inputDevices[busIndex]->channels[channel]->process(data, busBuffer.getNumSamples());
             float rmsLevel = juce::Decibels::gainToDecibels(busBuffer.getRMSLevel(getChannelIndexInProcessBlockBuffer(true, busIndex, channel), 0, busBuffer.getNumSamples()));
-            inChannels[channel]->setRMSLevel(rmsLevel);
+            inputDevices[busIndex]->channels[channel]->setRMSLevel(rmsLevel);
         }
     }
- //   rmsLevel = juce::Decibels::gainToDecibels(buffer.getRMSLevel(0, 0, buffer.getNumSamples()));
-
 }
 
 void MainMixer::releaseResources()
